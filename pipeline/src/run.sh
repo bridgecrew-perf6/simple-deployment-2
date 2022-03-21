a1ab61c1174c2e08c87784d67905fc2affcc4cac
oowibowo <only.wibowo@gmail.com>
2022-03-21 22:01:37 +0700

    Add files via upload

--- pipeline/src/run.sh
+++ pipeline/src/run.sh
@@ -4,11 +4,11 @@ runPipeline() {
 gst-launch-1.0 \
     rtspsrc location=rtsp://rtspsim:8554/stream1.mkv ! decodebin  ! videoconvert ! video/x-raw,format=BGRx ! \
     gvapython module=crowd.py class=InferenceTime ! \
-    gvainference model=model/dm_count/1/FP32/dm_count.xml inference-interval=5 ! \
+    gvainference model=model/dm_count/1/FP32/dm_count.xml inference-interval=5 device=GPU ! \
     gvapython module=crowd.py kwarg='{"stream": "100", "latitude": "-6.18188735639403", "longitude": "106.81674548492997"}' class=CrowdCount \
     rtspsrc location=rtsp://rtspsim:8554/stream2.mkv ! decodebin  ! videoconvert ! video/x-raw,format=BGRx ! \
     gvapython module=crowd.py class=InferenceTime ! \
-    gvainference model=model/dm_count/1/FP32/dm_count.xml inference-interval=5 ! \
+    gvainference model=model/dm_count/1/FP32/dm_count.xml inference-interval=5 device=GPU ! \
     gvapython module=crowd.py kwarg='{"stream": "101", "latitude": "-6.28182735639403", "longitude": "106.71670548492997"}' class=CrowdCount 
 
 return 1
